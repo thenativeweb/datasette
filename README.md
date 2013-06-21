f4a6b117b75b6e4b92261223342d84edab5e76d1
Golo Roden <webmaster@goloroden.de>
2013-06-21 07:20:05 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,86 @@
-datasette
-=========
+# datasette
 
 datasette is a key-value container for arbitrary data.
+
+## Installation
+
+At the moment, installation of this module must be made manually.
+
+## Quick start
+
+The first thing you need to do is to integrate datasette into your application. For that add a reference to the `datasette` module.
+
+```javascript
+var datasette = require('datasette');
+```
+
+Next, you can create a new data container. For that you need to call the `create` function.
+
+```javascript
+datasette.create(function (err, cc) {
+  // ...
+});
+```
+
+### Setting data
+
+To set data, call the data container's `set` function and specify the `key` and the `value` you want to store.
+
+```javascript
+cc.set('foo', { bar: 'baz' });
+```
+
+To set multiple keys and values at once, you can also hand over an object that contains the data.
+
+```javascript
+cc.set({
+  foo: 23,
+  bar: 42
+});
+```
+
+This is equivalent to calling `set` two times with separate key value pairs.
+
+### Getting data
+
+To get data, call the data container's `get` function and specify the `key` you would like to retrieve.
+
+```javascript
+var value = cc.get('foo');
+```
+
+*Note: Each time you call `get`, you will get a cloned result to avoid conflicting state changes on a shared reference.*
+
+### Dealing with events
+
+Every time a value is created, changed or deleted, the data container will emit a `changed` event. Use the `on` or `once` functions to subscribe to this event.
+
+cc.on('changed', function (key, value) {
+  // ...
+});
+
+If you are only interested in `changed` events for a specific `key`, subscribe to the `changed::*` event instead.
+
+cc.on('changed::foo', function (value) {
+  // ...
+});
+
+*Note: If you set the same key value pair two times, the data container will not emit an event on the second `set` call.*
+
+To unsubscribe from event notifications, use the `off` function.
+
+### Suppressing events
+
+If you don't want a `set` call to result in an emitted event, you can specify an `options` object and set its `silent` property to `true`.
+
+cc.set('foo', 'bar', { silent: true });
+
+## Running the tests
+
+datasette has been developed using TDD. To run the tests, go to the folder where you have installed datasette to and run `npm test`. You need to have [mocha](https://github.com/visionmedia/mocha) installed.
+
+    $ npm test
+
+Additionally, this module can be built using [Grunt](http://gruntjs.com/). Besides running the tests, Grunt also analyses the code using [JSHint](http://www.jshint.com/). To run Grunt, go to the folder where you have installed datasette and run `grunt`. You need to have [grunt-cli](https://github.com/gruntjs/grunt-cli) installed.
+
+    $ grunt
---
+++ test/datasetteTests.js
@@ -0,0 +1,206 @@
+'use strict';
+
+var assert = require('node-assertthat'),
+    cases = require('cases');
+
+var datasette = require('../lib/datasette');
+
+suite('datasette', function () {
+  suite('create', function () {
+    test('returns a new datasette instance.', function (done) {
+      datasette.create(function (err, cc) {
+        assert.that(err, is.null());
+        assert.that(cc.get, is.ofType('function'));
+        assert.that(cc.set, is.ofType('function'));
+        assert.that(cc.emit, is.ofType('function'));
+        assert.that(cc.on, is.ofType('function'));
+        assert.that(cc.once, is.ofType('function'));
+        assert.that(cc.off, is.ofType('function'));
+        done();
+      });
+    });
+  });
+
+  suite('get', function () {
+    test('returns undefined for a key that has not been set.', function (done) {
+      datasette.create(function (err, cc) {
+        assert.that(cc.get('foo'), is.undefined());
+        done();
+      });
+    });
+
+    test('returns the value for a key that has been set.', cases([
+      [ 'foo', 'bar' ],
+      [ 'bar', 23 ],
+      [ 'baz', true ],
+    ], function (key, value, done) {
+      datasette.create(function (err, cc) {
+        cc.set(key, value);
+        assert.that(cc.get(key), is.equalTo(value));
+        done();
+      });
+    }));
+
+    test('returns a new reference each time.', function (done) {
+      datasette.create(function (err, cc) {
+        cc.set('foo', { bar: 'baz' });
+        assert.that(cc.get('foo'), is.not.sameAs(cc.get('foo')));
+        done();
+      });
+    });
+  });
+
+  suite('set', function () {
+    test('stores the given key and value.', cases([
+      [ 'foo', 'bar' ],
+      [ 'bar', 23 ],
+      [ 'baz', true ],
+    ], function (key, value, done) {
+      datasette.create(function (err, cc) {
+        cc.set(key, value);
+        assert.that(cc.get(key), is.equalTo(value));
+        done();
+      });
+    }));
+
+    test('emits a changed event.', function (done) {
+      datasette.create(function (err, cc) {
+        var input = { bar: 'baz' };
+        cc.once('changed', function (key, value) {
+          assert.that(key, is.equalTo('foo'));
+          assert.that(value, is.equalTo(input));
+          done();
+        });
+        cc.set('foo', input);
+      });
+    });
+
+    test('emits a changed::* event.', cases([
+      [ 'foo' ],
+      [ 'bar' ],
+      [ 'baz' ],
+    ], function (key, done) {
+      datasette.create(function (err, cc) {
+        var value = { bar: 'baz' };
+        cc.once('changed::' + key, function (actual) {
+          assert.that(actual, is.equalTo(value));
+          done();
+        });
+        cc.set(key, value);
+      });
+    }));
+
+    test('does not emit a changed event if the value has not been changed.', function (done) {
+      datasette.create(function (err, cc) {
+        var input = { bar: 'baz' },
+            changedCounter = 0;
+        cc.set('foo', input);
+        cc.once('changed', function () {
+          changedCounter++;
+        });
+        cc.set('foo', input);
+        setTimeout(function () {
+          assert.that(changedCounter, is.equalTo(0));
+          done();
+        }, 25);
+      });
+    });
+
+    test('does not emit a changed event if silent is set to true.', function (done) {
+      datasette.create(function (err, cc) {
+        var changedCounter = 0;
+        cc.once('changed', function () {
+          changedCounter++;
+        });
+        cc.set('foo', 'bar', { silent: true });
+        setTimeout(function () {
+          assert.that(changedCounter, is.equalTo(0));
+          done();
+        }, 25);
+      });
+    });
+
+    test('does not emit a changed::* event if the value has not been changed.', function (done) {
+      datasette.create(function (err, cc) {
+        var input = { bar: 'baz' },
+            changedCounter = 0;
+        cc.set('foo', input);
+        cc.once('changed::foo', function () {
+          changedCounter++;
+        });
+        cc.set('foo', input);
+        setTimeout(function () {
+          assert.that(changedCounter, is.equalTo(0));
+          done();
+        }, 25);
+      });
+    });
+
+    test('does not emit a changed::* event if silent is set to true.', function (done) {
+      datasette.create(function (err, cc) {
+        var changedCounter = 0;
+        cc.set('foo', { bar: 'baz' });
+        cc.once('changed::foo', function () {
+          changedCounter++;
+        });
+        cc.set('foo', { bar: 'baz' }, { silent: true });
+        setTimeout(function () {
+          assert.that(changedCounter, is.equalTo(0));
+          done();
+        }, 25);
+      });
+    });
+
+    test('hands over a new reference on a changed event.', function (done) {
+      datasette.create(function (err, cc) {
+        var input = { bar: 'baz' };
+        cc.once('changed', function (key, value) {
+          assert.that(value, is.not.sameAs(input));
+          done();
+        });
+        cc.set('foo', input);
+      });
+    });
+
+    test('hands over a new reference on a changed::* event.', function (done) {
+      datasette.create(function (err, cc) {
+        var input = { bar: 'baz' };
+        cc.once('changed::foo', function (value) {
+          assert.that(value, is.not.sameAs(input));
+          done();
+        });
+        cc.set('foo', input);
+      });
+    });
+
+    test('updates a key that had been set before.', function (done) {
+      datasette.create(function (err, cc) {
+        cc.set('foo', 'bar');
+        cc.set('foo', 'baz');
+        assert.that(cc.get('foo'), is.equalTo('baz'));
+        done();
+      });
+    });
+
+    test('removes a key when value is missing.', function (done) {
+      datasette.create(function (err, cc) {
+        cc.set('foo', 'bar');
+        cc.set('foo');
+        assert.that(cc.get('foo'), is.undefined());
+        done();
+      });
+    });
+
+    test('sets multiple key value pairs at once.', function (done) {
+      datasette.create(function (err, cc) {
+        cc.set({
+          foo: 'bar',
+          baz: 'bat'
+        });
+        assert.that(cc.get('foo'), is.equalTo('bar'));
+        assert.that(cc.get('baz'), is.equalTo('bat'));
+        done();
+      });
+    });
+  });
+});
